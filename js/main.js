9080871b304741b46f8440217853d862f6915d73
Giuseppe Suanno <suanno.giuseppe@yahoo.it>
2023-02-22 19:10:50 +0100

    Aggiunto parte mancante dell'esercizio

--- js/main.js
+++ js/main.js
@@ -6,15 +6,21 @@ let somma = km * prezzo
 if(anni < 18 ){
     let sconto = somma * 20 / 100
     somma= somma - sconto
+    somma = somma.toFixed();
+    document.getElementById("sconto1").innerHTML="Sconto ragazzi under 18 : " + somma;
+
     
     
 } else if (anni >= 66 ) {
     let sconto = somma * 40 / 100
     somma= somma - sconto
+    somma = somma.toFixed();
+    document.getElementById("sconto1").innerHTML="Sconto over 65 : " + somma;
    
 
 }else{
     console.log(somma)
+    somma = somma.toFixed();
 }
 
 document.getElementById("totale").innerHTML="Il totale del prezzo del biglietto è: " + somma;
