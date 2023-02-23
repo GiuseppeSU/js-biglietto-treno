a4ec3d626c739a8eae49570533883ee9201bb76e
Giuseppe Suanno <suanno.giuseppe@yahoo.it>
2023-02-23 10:26:25 +0100

    aggiunto simbolo euro e il numer 2 nel tofixed

--- js/main.js
+++ js/main.js
@@ -6,24 +6,24 @@ let somma = km * prezzo
 if(anni < 18 ){
     let sconto = somma * 20 / 100
     somma= somma - sconto
-    somma = somma.toFixed();
-    document.getElementById("sconto1").innerHTML="Sconto ragazzi under 18 : " + somma;
+    somma = somma.toFixed(2);
+    document.getElementById("sconto1").innerHTML="Sconto ragazzi under 18 : " + sconto.toFixed(2) + "€";
 
     
     
 } else if (anni >= 66 ) {
     let sconto = somma * 40 / 100
     somma= somma - sconto
-    somma = somma.toFixed();
-    document.getElementById("sconto1").innerHTML="Sconto over 65 : " + somma;
+    somma = somma.toFixed(2)  ;
+    document.getElementById("sconto1").innerHTML="Sconto over 65 : " + sconto.toFixed(2) + "€" ;
    
 
 }else{
     console.log(somma)
-    somma = somma.toFixed();
+    somma = somma.toFixed(2);
 }
 
-document.getElementById("totale").innerHTML="Il totale del prezzo del biglietto è: " + somma;
+document.getElementById("totale").innerHTML="Il totale del prezzo del biglietto è: " + somma + "€";
 
 
 
